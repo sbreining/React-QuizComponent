f91506c54bd07757a9d997350c2a0ca18b033499
sbreining <sbreining@paylease.com>
2020-05-08 15:39:39 -0700

    Module6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { incorrectAnswer: false };
+  }
+
   handleClick(buttonText) {
+    let incorrectAnswer = true;
     if (buttonText === this.props.quiz_question.answer) {
+      let incorrectAnswer = false;
       this.props.showNextQuestionHandler();
     }
+
+    this.setState({ incorrectAnswer });
+
   }
 
   render() {
@@ -27,6 +38,7 @@ class QuizQuestion extends Component {
             })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
     );
   }
